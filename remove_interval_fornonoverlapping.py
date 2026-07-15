e3733efec4a93eeff327a9c54ae1e35bb273f5c6
anoop <eee.anoop@gmail.com>
2026-07-14 20:20:44 -0700

    non overlaping intervals

--- remove_interval_fornonoverlapping.py
+++ remove_interval_fornonoverlapping.py
@@ -1,23 +1,25 @@
+
+
+
 def count_overlapping_intevals(intervals: list[list[int]]) -> int:
-  overlapping_interval_count: int = 0
-  non_overlapping_intv: list[list[int]] = []
-  intervals.sort(key=lambda x : x[0])
+    overlapping_interval_count: int = 0
+    non_overlapping_intv: list[list[int]] = []
+    intervals.sort(key=lambda x : x[0])
 
-  for curr_start, curr_end in intervals:
-    if not non_overlapping_intv:
-      non_overlapping_intv.append([curr_start, curr_end])
-      print(f'First time {[curr_start, curr_end]}')
+    for curr_start, curr_end in intervals:
+        if not non_overlapping_intv:
+            non_overlapping_intv.append([curr_start, curr_end])
+            print(f'First time {[curr_start, curr_end]}')
 
-    else:
-      if curr_start >= non_overlapping_intv[-1][1]:
-        non_overlapping_intv.append([curr_start, curr_end])
-        print(f'Second onwards {[curr_start, curr_end]}')
+        else:
+            if curr_start >= non_overlapping_intv[-1][1]:
+                non_overlapping_intv.append([curr_start, curr_end])
+                print(f'Second onwards {[curr_start, curr_end]}')
+            
+    print(non_overlapping_intv)
+
+    overlapping_interval_count = len(intervals) - len(non_overlapping_intv)
         
-  print(non_overlapping_intv)
-
-  overlapping_interval_count = len(intervals) - len(non_overlapping_intv)
-    
-  return overlapping_interval_count
+    return overlapping_interval_count
 
 
-print(count_overlapping_intevals([,[1,11],[2,12],[11,22],[1,100]]))
